ff85550811f3de57b4c23d9696bf959781ce9f2f
EGOIST <0x142857@gmail.com>
2018-10-24 19:27:56 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: 'Poi',
+  description: 'Delightful web development.',
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Guide',
+        collapsable: false,
+        children: [
+          '/guide/getting-started',
+          '/guide/transforms',
+          '/guide/serve-public-files'
+        ]
+      }
+    ],
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/getting-started'
+      },
+      {
+        text: 'Config File',
+        link: '/config'
+      },
+      {
+        text: 'Plugin Dev Guide',
+        link: '/plugin-dev'
+      }
+    ],
+    editLinks: true,
+    repo: 'egoist/poi',
+    docsRepo: 'poi-bundler/website',
+    docsDir: 'docs'
+  }
+}
---
+++ docs/.vuepress/theme/VersionSelector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="version-selector">
+    <select @change="handleSelect">
+      <option value="v11" selected>v11</option>
+      <option value="v10">v10</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSelect(e) {
+      if (e.target.value === 'v10') {
+        window.location.href = 'https://v10--poi.netlify.com/'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.version-selector {
+  padding: 1.5rem 1.5rem 0;
+}
+</style>
+
